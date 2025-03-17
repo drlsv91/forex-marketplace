92faa618e5a19f84d0796c715562df4c9d1af4d5
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-03-17 18:43:03 +0100

    feat: implement authentication module with login functionality and configuration validation

--- apps/user-auth-service/src/app/app.module.ts
+++ apps/user-auth-service/src/app/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { HealthModule } from '@forex-marketplace/common';
+import {
+  DatabaseModule,
+  HealthModule,
+  LoggerModule,
+} from '@forex-marketplace/common';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 @Module({
-  imports: [UsersModule, HealthModule],
+  imports: [
+    LoggerModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION: Joi.string().required(),
+      }),
+    }),
+    HealthModule,
+    UsersModule,
+    AuthModule,
+  ],
+
   controllers: [],
   providers: [AppService],
 })
---
+++ apps/user-auth-service/src/app/auth/auth.controller.ts
@@ -0,0 +1,30 @@
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { UserEntity } from '../users/entities/user.entity';
+import { currentUser } from '@forex-marketplace/common';
+import { Response } from 'express';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @UseGuards(LocalAuthGuard)
+  async login(
+    @Body() loginDto: LoginDto,
+    @currentUser() user: UserEntity,
+    @Res({ passthrough: true }) response: Response
+  ) {
+    const result = await this.authService.login(user, response);
+    response.status(HttpStatus.OK).send(result);
+  }
+}
