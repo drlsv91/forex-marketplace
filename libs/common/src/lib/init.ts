5fdcf3946195bb9789429649cb9e86f678d45ebe
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-03-17 19:09:53 +0100

    feat: enhance initApp function to support Swagger documentation configuration

--- libs/common/src/lib/init.ts
+++ libs/common/src/lib/init.ts
@@ -2,12 +2,37 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-export async function initApp(app: INestApplication, globalPrefix = 'api') {
+export async function initApp(
+  app: INestApplication,
+  options: {
+    globalPrefix?: string;
+    docs: {
+      title: string;
+      description: string;
+      tagName: string;
+      endpoint?: string;
+    };
+  }
+) {
+  const { globalPrefix = 'api', docs } = options;
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.setGlobalPrefix(globalPrefix);
   app.useLogger(app.get(Logger));
   app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle(docs.title)
+    .setDescription(docs.description)
+    .setVersion('1.0')
+    .addTag(docs.tagName)
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(docs.endpoint ?? 'docs', app, document);
+
   const port = app.get(ConfigService).getOrThrow('PORT');
   await app.listen(port);
   app
