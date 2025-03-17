9620aeaa1030f4e34733c0ad6524999713b0d82d
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-03-17 15:56:57 +0100

    feat: implement user module with controller, service, and DTOs

--- apps/user-auth-service/src/app/app.module.ts
+++ apps/user-auth-service/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UsersModule } from './users/users.module';
+import { HealthModule } from '@forex-marketplace/common';
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UsersModule, HealthModule],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
