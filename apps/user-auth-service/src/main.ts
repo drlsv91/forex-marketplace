2baee93fc2ddcee9d9ece0487d9429de9030be19
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-03-18 22:19:25 +0100

    feat: configure gRPC transport with AUTH_GRPC_URL and reorder initialization

--- apps/user-auth-service/src/main.ts
+++ apps/user-auth-service/src/main.ts
@@ -4,14 +4,25 @@
  */
 
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app/app.module';
 import { initApp } from '@forex-marketplace/common';
-import { GrpcOptions, Transport } from '@nestjs/microservices';
 import { AUTH_PACKAGE_NAME } from 'types/proto/auth';
 import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  app.connectMicroservice<GrpcOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: AUTH_PACKAGE_NAME,
+      protoPath: join(__dirname, 'proto/auth.proto'),
+      url: app.get(ConfigService).getOrThrow('AUTH_GRPC_URL'),
+    },
+  });
+
   await initApp(app, {
     docs: {
       title: 'User Auth Service',
@@ -19,13 +30,7 @@ async function bootstrap() {
       tagName: 'Auth Service',
     },
   });
-  app.connectMicroservice<GrpcOptions>({
-    transport: Transport.GRPC,
-    options: {
-      package: AUTH_PACKAGE_NAME,
-      protoPath: join(__dirname, 'proto/auth.proto'),
-    },
-  });
+
   app.startAllMicroservices();
 }
 
