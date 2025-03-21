a87157c7763613654ef867965774effbe04a179f
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-03-21 19:07:50 +0100

    chore: add transformation to lowercase and remove spaces for email input in LoginDto

--- apps/user-auth-service/src/app/auth/dto/login.dto.ts
+++ apps/user-auth-service/src/app/auth/dto/login.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsEmail, IsStrongPassword } from 'class-validator';
 
 export class LoginDto {
@@ -7,6 +8,7 @@ export class LoginDto {
     description: 'User email',
     example: 'john.doe@google.com',
   })
+  @Transform(({ value }) => value.toLowerCase().replace(/\s/g, ''))
   email: string;
 
   @IsStrongPassword()
